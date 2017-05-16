6094cacda6c05e65c8420f0cecf8f76334e42976
Danielle Stiring <deerextech@gmail.com>
2017-05-15 23:08:04 -0400

    All i did was take out commented code. Oh, and in the heat of the moment (last commit) forgot to mention that ENCOUDEDURL and YARGS now live in app.js again. So. Two things to remember: 1. Yargs/EncodedURL are in app.js 2. RESOLVE AND REJECT ONLY TAKE 1 ARGUMENT.
    Deal with weather restructure tomorrow. It is late. And you have drawings to work on.
    And are starting to talk to yourself in git commit messages...seems like it's quitting time.
    
    	modified:   async-weather-app/app.js
    	modified:   async-weather-app/components/config/geocode.js

--- async-weather-app/app.js
+++ async-weather-app/app.js
@@ -6,22 +6,6 @@ const yargs = require('./components/config/yargsConfig');
 const argv = yargs.argv;
 var encodedAddress = encodeURIComponent(argv.address);
 
-//
-// geocode.geocodeAddress((errorMessage, results)=>{
-//   if(errorMessage){
-//     console.log('errorMessage:', errorMessage)
-//   }else{
-//     console.log('WEATHER FOR: ', results.address);
-//     weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
-//       if(errorMessage){
-//         console.log('error:', errorMessage);
-//       }else{
-//         console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemp}`)
-//       }
-//     });
-//   }
-//
-// });
 
 geocode.geocodeAddress(encodedAddress).then((results)=>{
   console.log('RESULT: ', results);
@@ -35,9 +19,3 @@ geocode.geocodeAddress(encodedAddress).then((results)=>{
 }).catch((errorMessage)=>{
   console.log('did not work');
 })
-// geocodeAddress(encodedAddress, callback).then((result)=>{
-//   console.log('SHOW ME ENCODED! ', encodedAddress);
-//   console.log('RESULT: ', result)
-// }).catch((errorMessage)=>{
-//   console.log('didnt wurk ');
-// })
--- async-weather-app/components/config/geocode.js
+++ async-weather-app/components/config/geocode.js
@@ -23,39 +23,6 @@ var geocodeAddress = (address) =>{
   })
 }
 
-// geocodeAddress(encodedAddress, callback).then((result)=>{
-//   console.log('WTF', encodedAddress);
-//   console.log('RESULT: ', result)
-// }).catch((errorMessage)=>{
-//   console.log('didnt wurk');
-// })
-
-// var geocodeAddress = (callback) =>{
-//   const argv = yargs.argv;
-//
-//   var encodedAddress = encodeURIComponent(argv.address);
-//
-//   //make request
-//   request({
-//     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-//
-//     //url:'http://maps.googleapis.com/maps/api/geocode/json?address=730OntarioStreetToronto',
-//     json:true
-//   },(error, response, body)=>{
-//     if(error){
-//       callback('unable to connect to Google servers.');
-//     }else if(body.status === 'ZERO_RESULTS'){
-//       callback('unable to find that address');
-//     }else if(body.status === 'OK'){
-//       callback(undefined, {
-//         address: body.results[0].formatted_address,
-//         latitude:body.results[0].geometry.location.lat,
-//         longitude: body.results[0].geometry.location.lng
-//       });
-//     }
-//   });
-// }
-
 module.exports ={
   geocodeAddress
 }
