91b2154a77e23e5b70b85d83d2cb7476cf042e69
Danielle Stiring <deerextech@gmail.com>
2017-05-15 22:57:25 -0400

    First promise set up for getting location theeeeen getting temperature from that. Next up: Weather refactor.
    Future Dee: Do not forget to restructure success callback (when you change it to resolve) and REMOVE UNDEFINED ARGUMENT.You know.. that thing that doesn't belong there... that screwed you for like 20 minutes. Remember that? RESOLVE/REJECT can only take ONE argument. ONE.
    
    Never forget.
    
    Also, I left in previous structure in geocode... just for an extra reference in git logs. I know. Overkill. Whatever tho. That error sucked to figure out.
    
    	modified:   async-weather-app/app.js
    	modified:   async-weather-app/components/config/geocode.js

--- async-weather-app/app.js
+++ async-weather-app/app.js
@@ -1,20 +1,43 @@
 const geocode = require('./components/config/geocode');
 const weather = require('./components/weather/weather');
+const yargs = require('./components/config/yargsConfig');
 
 
+const argv = yargs.argv;
+var encodedAddress = encodeURIComponent(argv.address);
 
-geocode.geocodeAddress((errorMessage, results)=>{
-  if(errorMessage){
-    console.log('errorMessage:', errorMessage)
-  }else{
-    console.log('WEATHER FOR: ', results.address);
-    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
-      if(errorMessage){
-        console.log('error:', errorMessage);
-      }else{
-        console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemp}`)
-      }
-    });
-  }
+//
+// geocode.geocodeAddress((errorMessage, results)=>{
+//   if(errorMessage){
+//     console.log('errorMessage:', errorMessage)
+//   }else{
+//     console.log('WEATHER FOR: ', results.address);
+//     weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
+//       if(errorMessage){
+//         console.log('error:', errorMessage);
+//       }else{
+//         console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemp}`)
+//       }
+//     });
+//   }
+//
+// });
 
-});
+geocode.geocodeAddress(encodedAddress).then((results)=>{
+  console.log('RESULT: ', results);
+  weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
+    if(errorMessage){
+      console.log('error:', errorMessage);
+    }else{
+      console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemp}`)
+    }
+  })
+}).catch((errorMessage)=>{
+  console.log('did not work');
+})
+// geocodeAddress(encodedAddress, callback).then((result)=>{
+//   console.log('SHOW ME ENCODED! ', encodedAddress);
+//   console.log('RESULT: ', result)
+// }).catch((errorMessage)=>{
+//   console.log('didnt wurk ');
+// })
--- async-weather-app/components/config/geocode.js
+++ async-weather-app/components/config/geocode.js
@@ -1,31 +1,60 @@
 const request = require('request');
 const yargs = require('./yargsConfig');
 
-var geocodeAddress = (callback) =>{
-  const argv = yargs.argv;
-
-  var encodedAddress = encodeURIComponent(argv.address);
-
-  //make request
-  request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-
-    //url:'http://maps.googleapis.com/maps/api/geocode/json?address=730OntarioStreetToronto',
-    json:true
-  },(error, response, body)=>{
-    if(error){
-      callback('unable to connect to Google servers.');
-    }else if(body.status === 'ZERO_RESULTS'){
-      callback('unable to find that address');
-    }else if(body.status === 'OK'){
-      callback(undefined, {
-        address: body.results[0].formatted_address,
-        latitude:body.results[0].geometry.location.lat,
-        longitude: body.results[0].geometry.location.lng
-      });
-    }
-  });
+var geocodeAddress = (address) =>{
+  return new Promise((resolve,reject)=> {
+      //make request
+    request({
+      url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
+      json:true
+    },(error, response, body)=>{
+      if(error){
+        reject('unable to connect to Google servers.');
+      }else if(body.status === 'ZERO_RESULTS'){
+        reject('unable to find that address');
+      }else if(body.status === 'OK'){
+        resolve({
+          address: body.results[0].formatted_address,
+          latitude:body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    });
+  })
 }
+
+// geocodeAddress(encodedAddress, callback).then((result)=>{
+//   console.log('WTF', encodedAddress);
+//   console.log('RESULT: ', result)
+// }).catch((errorMessage)=>{
+//   console.log('didnt wurk');
+// })
+
+// var geocodeAddress = (callback) =>{
+//   const argv = yargs.argv;
+//
+//   var encodedAddress = encodeURIComponent(argv.address);
+//
+//   //make request
+//   request({
+//     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+//
+//     //url:'http://maps.googleapis.com/maps/api/geocode/json?address=730OntarioStreetToronto',
+//     json:true
+//   },(error, response, body)=>{
+//     if(error){
+//       callback('unable to connect to Google servers.');
+//     }else if(body.status === 'ZERO_RESULTS'){
+//       callback('unable to find that address');
+//     }else if(body.status === 'OK'){
+//       callback(undefined, {
+//         address: body.results[0].formatted_address,
+//         latitude:body.results[0].geometry.location.lat,
+//         longitude: body.results[0].geometry.location.lng
+//       });
+//     }
+//   });
+// }
 
 module.exports ={
   geocodeAddress
